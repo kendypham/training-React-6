ea927354d1a821289aa38a6c4b27eb21bf30f12c
Truong Pham Nhat <pntruong1@tma.com.vn>
2019-09-05 17:40:28 +0700

    fix ui

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -6,29 +6,29 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
-          <Navbar.Brand href="#home" className="flex-grow-1 text-center">Contact us</Navbar.Brand>
-          <Nav.Link href="#link" className="fas fa-sign-in-alt text-uppercase text-light"> Login/Signup</Nav.Link>
+        <Navbar  variant="dark" className="d-flex justify-content-between bg-nav">
+          <Navbar.Brand href="#home" className="flex-grow-1 text-center text-uppercase text-nav">Contact us</Navbar.Brand>
+          <i className="fas fa-sign-in-alt icon text-light"></i> <Nav.Link href="#link" className="text-uppercase text-light text-icon">Login/Signup</Nav.Link>
         </Navbar>
         <Container fluid={true}>
           <Row className="justify-content-md-center align-items-md-center mt-5">
             <Col lg="auto">
               <Card style={{ width: '25rem' }}> 
-                <Card.Header className="font-weight-bold h3">Login</Card.Header>
+                <Card.Header className="login-text">Login</Card.Header>
                 <Card.Body>
                   <Form>
                     <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Label className="text-uppercase label">Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Email address" />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
+                      <Form.Label className="text-uppercase label">Password</Form.Label>
                       <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
-                    <Button variant="success" type="submit" className="text-uppercase">
+                    <button  type="submit" className="btn text-uppercase btn-color">
                       Login
-                  </Button>
-                  <Card.Link href="#" className="float-right text-uppercase text-dark h5">Forgot Password ?</Card.Link>
+                  </button>
+                  <Card.Link href="#" className="float-right text-uppercase text-forgot">Forgot Password ?</Card.Link>
                   </Form>
                 </Card.Body>
               </Card>
